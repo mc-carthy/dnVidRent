180cd95d93e7e9710bd7b7189c5d8916244bbff4
mc-carthy <mccarthy.mick0012@gmail.com>
2017-07-07 17:47:25 +0100

    Clicking customer entries on index page takes the user to a details page for that customer

--- VidRen/Controllers/CustomersController.cs
+++ VidRen/Controllers/CustomersController.cs
@@ -16,6 +16,18 @@ namespace VidRen.Controllers
             return View(customers);
         }
 
+        public ActionResult Details(int id)
+        {
+            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(customer);
+        }
+
         private IEnumerable<Customer> GetCustomers()
         {
             return new List<Customer>
