a53ba01f1dfec076ad835643c5a6df1dc0c5d079
mc-carthy <mccarthy.mick0012@gmail.com>
2017-07-07 17:54:59 +0100

    Implement index page for movies with hard-coded movie entries

--- VidRen/Controllers/MoviesController.cs
+++ VidRen/Controllers/MoviesController.cs
@@ -11,6 +11,14 @@ namespace VidRen.Controllers
     public class MoviesController : Controller
     {
         // GET: Movies
+        public ActionResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
@@ -29,30 +37,13 @@ namespace VidRen.Controllers
             return View(viewModel);
         }
 
-        public ActionResult Edit(int id)
+        private IEnumerable<Movie> GetMovies()
         {
-            return Content("id=" + id);
-        }
-
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
+            return new List<Movie>
             {
-                pageIndex = 1;
-            }
-
-            if (String.IsNullOrWhiteSpace(sortBy))
-            {
-                sortBy = "Name";
-            }
-
-            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
-        }
-
-        [Route("movies/released/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1,12)}")]
-        public ActionResult ByReleaseDate(int year, int month)
-        {
-            return Content(year + "/" + month);
+                new Movie { Id = 1, Name = "Shrek"},
+                new Movie { Id = 2, Name = "Wall-E" }
+            };
         }
     }
 }
