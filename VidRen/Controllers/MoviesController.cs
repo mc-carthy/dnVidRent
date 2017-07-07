8b45a5abccfcb8e7d9cde6c425682bc04b65350e
mc-carthy <mccarthy.mick0012@gmail.com>
2017-07-07 16:42:49 +0100

    Using action parameters

--- VidRen/Controllers/MoviesController.cs
+++ VidRen/Controllers/MoviesController.cs
@@ -15,5 +15,25 @@ namespace VidRen.Controllers
             var movie = new Movie() { Name = "Shrek!" };
             return View(movie);
         }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
     }
 }
