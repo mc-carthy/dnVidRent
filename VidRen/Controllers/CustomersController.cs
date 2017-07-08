4c765fa24999082508e769138809220a441d1dd7
mc-carthy <mccarthy.mick0012@gmail.com>
2017-07-08 16:38:44 +0100

    Implement edit customer form

--- VidRen/Controllers/CustomersController.cs
+++ VidRen/Controllers/CustomersController.cs
@@ -46,12 +46,12 @@ namespace VidRen.Controllers
         public ActionResult New()
         {
             var membershipTypes = _context.MembershipTypes.ToList();
-            var viewModel = new NewCustomerViewModel
+            var viewModel = new CustomerFormViewModel
             {
                 MembershipTypes = membershipTypes
             };
 
-            return View(viewModel);
+            return View("CustomerForm", viewModel);
         }
 
         [HttpPost]
@@ -61,6 +61,24 @@ namespace VidRen.Controllers
             _context.SaveChanges();
             
             return RedirectToAction("Index", "Customers");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+
+            return View("CustomerForm", viewModel);
         }
 
     }
--- VidRen/ViewModels/CustomerFormViewModel.cs
+++ VidRen/ViewModels/CustomerFormViewModel.cs
@@ -6,7 +6,7 @@ using VidRen.Models;
 
 namespace VidRen.ViewModels
 {
-    public class NewCustomerViewModel
+    public class CustomerFormViewModel
     {
         public IEnumerable<MembershipType> MembershipTypes { get; set; }
         public Customer Customer { get; set; }
